ba3df980824ae252101b16d605b097f3a0c2db42
Guilherme Santos <guiromanno@gmail.com>
2021-07-13 13:41:43 -0300

    Deletando e Atualizando os Clientes

--- src/main/java/br/com/systemsgs/servicos/controller/ClienteController.java
+++ src/main/java/br/com/systemsgs/servicos/controller/ClienteController.java
@@ -33,4 +33,18 @@ public class ClienteController {
         return clienteRepository.findById(id).orElseThrow(() -> new RecursoNaoEncontradoException());
     }
 
+    @PutMapping(value = "/atualiza/{id}")
+    public String atualizaCliente(@PathVariable("id") Long id, @RequestBody ModelCliente modelCliente){
+        clienteRepository.findById(id).map(cliente -> {modelCliente.setId(cliente.getId()); return clienteRepository.save(modelCliente);}).orElseThrow(() -> new RecursoNaoEncontradoException());
+
+        return "Cliente Atualizado com Sucesso!!!";
+    }
+
+    @DeleteMapping(value = "/deletar/{id}")
+    public String deletaCliente(@PathVariable("id") Long id){
+        clienteRepository.findById(id).map(cliente -> {clienteRepository.delete(cliente); return Void.TYPE;}).orElseThrow(() -> new RecursoNaoEncontradoException());
+
+        return "Cliente Deletado com Sucesso!!!";
+    }
+
 }
