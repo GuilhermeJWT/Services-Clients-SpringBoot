e55b0aaed1c43aca1abceec871dc9c0fd2ab059d
Guilherme Santos <guiromanno@gmail.com>
2021-07-13 12:22:43 -0300

    Conexão com Postgres e criando o ModelCliente

---
+++ src/main/java/br/com/systemsgs/servicos/model/ModelCliente.java
@@ -0,0 +1,33 @@
+package br.com.systemsgs.servicos.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "cliente")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ModelCliente implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O Nome deve ser Informado!!!")
+    private String nome;
+
+    @NotBlank(message = "O CPF Deve ser Informado!!!")
+    @CPF(message = "CPF Inválido!!!")
+    private String cpf;
+
+    private LocalDate dataCadastro;
+
+}
